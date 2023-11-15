c32f1584e3ba87de08f9d2377c09b1b7c847c94b
Felipe Dos Santos <felipe.santosmf2022@gmail.com>
2023-11-15 16:07:48 -0300

    Class Event to Timer point system

---
+++ src/main/java/com/felipesantos/warmine/events/CityTimerEvent.java
@@ -0,0 +1,39 @@
+package com.felipesantos.warmine.events;
+
+import com.felipesantos.warmine.WarMine;
+import com.felipesantos.warmine.entities.*;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = WarMine.MOD_ID)
+public class CityTimerEvent {
+
+    private static int ticksElapsed = 0;
+    private static final int TICKS_PER_MINUTE = 1200;
+
+    @SubscribeEvent
+    public static void onCityTimer(TickEvent.ServerTickEvent event){
+        if (event.phase == TickEvent.Phase.START) {
+            ticksElapsed++;
+            if(ticksElapsed >= 30*TICKS_PER_MINUTE){
+                incrementPoints();
+                ticksElapsed = 0;
+            }
+        }
+    }
+
+    private static void incrementPoints(){
+        if(MinecraftData.warmine.getCities() != null){
+            for(CityDataBlock cityBlock : MinecraftData.warmine.getCities()){
+                if(cityBlock.getWarTeam() != null){
+                    if(WarMineData.teamAlreadyHaveACapital(cityBlock.getWarTeam())){
+                        cityBlock.getWarTeam().incrementScore(1);
+                    }
+                }
+            }
+        }
+
+    }
+
+}
---
+++ src/main/java/com/felipesantos/warmine/events/CrownTimerEvent.java
@@ -0,0 +1,39 @@
+package com.felipesantos.warmine.events;
+
+import com.felipesantos.warmine.WarMine;
+import com.felipesantos.warmine.entities.CrownDataBlock;
+import com.felipesantos.warmine.entities.MinecraftData;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = WarMine.MOD_ID)
+public class CrownTimerEvent {
+
+    private static int ticksElapsed = 0;
+    private static final int TICKS_PER_MINUTE = 1200;
+
+    @SubscribeEvent
+    public static void onCrownTimer(TickEvent.ServerTickEvent event){
+        if (event.phase == TickEvent.Phase.START) {
+            ticksElapsed++;
+            if(ticksElapsed >= 10*TICKS_PER_MINUTE){
+                incrementPoints();
+                ticksElapsed = 0;
+            }
+        }
+    }
+
+    private static void incrementPoints(){
+        System.out.println("LOG | INCREMENT CAPITALS");
+        if(MinecraftData.warmine.getCapitals() != null){
+            for(CrownDataBlock crownBlock : MinecraftData.warmine.getCapitals()){
+                if(crownBlock.getWarTeam() != null){
+                    crownBlock.getWarTeam().incrementScore(1);
+                }
+            }
+        }
+
+    }
+}
